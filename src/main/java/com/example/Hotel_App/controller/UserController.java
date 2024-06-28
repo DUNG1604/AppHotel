6f923798a24807ef4a598eac512dbe9f5b179f85
DUNG1604 <dung0373691479@gmail.com>
2024-06-28 16:58:10 +0700

    add

--- src/main/java/com/example/Hotel_App/controller/UserController.java
+++ src/main/java/com/example/Hotel_App/controller/UserController.java
@@ -2,13 +2,15 @@ package com.example.Hotel_App.controller;
 
 import com.example.Hotel_App.dto.request.UserCreationRequest;
 import com.example.Hotel_App.entity.User;
+import com.example.Hotel_App.repository.UserRepository;
 import com.example.Hotel_App.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -17,4 +19,9 @@ public class UserController {
     User createUser(@RequestBody UserCreationRequest request){
         return userService.createUser(request);
     }
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
 }
--- src/main/java/com/example/Hotel_App/service/UserService.java
+++ src/main/java/com/example/Hotel_App/service/UserService.java
@@ -6,6 +6,8 @@ import com.example.Hotel_App.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -20,5 +22,9 @@ public class UserService {
         user.setLastname(request.getLastname());
 
         return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
